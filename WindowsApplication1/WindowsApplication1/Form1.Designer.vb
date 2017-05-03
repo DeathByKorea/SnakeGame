c052431f9d9a45955cf89277da6a458b1efabf45
Patalayus <lukecharles222@gmail.com>
2017-05-03 18:48:53 +0100

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tm_snakeMover = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.pb_field = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb_field, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tm_snakeMover
+        '
+        Me.tm_snakeMover.Interval = 60
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(1138, 2)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 18)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "by Luke Charles"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.WindowsApplication1.My.Resources.Resources.sjdkal_s
+        Me.PictureBox4.Location = New System.Drawing.Point(974, 23)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(268, 63)
+        Me.PictureBox4.TabIndex = 6
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.snakelogofinished
+        Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PictureBox3.Location = New System.Drawing.Point(239, 2)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(489, 100)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.applev4
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Location = New System.Drawing.Point(712, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(79, 79)
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'pb_field
+        '
+        Me.pb_field.BackColor = System.Drawing.Color.Black
+        Me.pb_field.Location = New System.Drawing.Point(10, 108)
+        Me.pb_field.Name = "pb_field"
+        Me.pb_field.Size = New System.Drawing.Size(950, 549)
+        Me.pb_field.TabIndex = 0
+        Me.pb_field.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(1254, 670)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.pb_field)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb_field, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pb_field As System.Windows.Forms.PictureBox
+    Friend WithEvents tm_snakeMover As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+
+#Region "Snake Stuff"
+    Dim snake(1000) As PictureBox
+    Dim length_of_snake As Integer = -1
+    Dim left_right_mover As Integer = 0
+    Dim up_down_mover = 0
+    Dim r As New Random
+    Private Sub create_head()
+        length_of_snake += 1
+        snake(length_of_snake) = New PictureBox
+        With snake(length_of_snake)
+            .Height = 10
+            .Width = 10
+            .BackColor = Color.Lime
+            .Top = (pb_field.Top + pb_field.Bottom) / 2
+            .Left = (pb_field.Left + pb_field.Right) / 2
+
+        End With
+        Me.Controls.Add(snake(length_of_snake))
+        snake(length_of_snake).BringToFront()
+
+        lengthensnake()
+        lengthensnake()
+    End Sub
+#End Region
+#Region "Collision"
+    Private Sub collide_with_walls()
+        If snake(0).Left < pb_field.Left Then
+            tm_snakeMover.Stop()
+            MsgBox("GAME OVER")
+        End If
+        If snake(0).Right > pb_field.Right Then
+            tm_snakeMover.Stop()
+            MsgBox("GAME OVER")
+        End If
+        If snake(0).Top < pb_field.Top Then
+            tm_snakeMover.Stop()
+            MsgBox("GAME OVER")
+        End If
+        If snake(0).Bottom > pb_field.Bottom Then
+            tm_snakeMover.Stop()
+            MsgBox("GAME OVER")
+        End If
+    End Sub
+
+    Private Sub collide_with_mouse()
+        If snake(0).Bounds.IntersectsWith(mouse.Bounds) Then
+            lengthensnake()
+            mouse.Top = r.Next
+            mouse.Top = r.Next(pb_field.Top, pb_field.Bottom - 10)
+            mouse.Left = r.Next(pb_field.Left, pb_field.Right - 10)
+        End If
+    End Sub
+#End Region
+#Region "Mouse Movement"
+    Dim mouse As PictureBox
+    Private Sub create_mouse()
+        mouse = New PictureBox
+        With mouse
+            .Width = 10
+            .Height = 10
+            .BackColor = Color.Red
+
+            .Top = r.Next(pb_field.Top, pb_field.Bottom - 10)
+            .Left = r.Next(pb_field.Left, pb_field.Right - 10)
+
+        End With
+        Me.Controls.Add(mouse)
+        mouse.BringToFront()
+    End Sub
+#End Region
+    Private Sub lengthensnake()
+        length_of_snake += 1
+        snake(length_of_snake) = New PictureBox
+        With snake(length_of_snake)
+            .Height = 10
+            .Width = 10
+            .BackColor = Color.Lime
+            .Top = snake(length_of_snake - 1).Top
+            .Left = snake(length_of_snake - 1).Left + 10
+        End With
+        Me.Controls.Add(snake(length_of_snake))
+        snake(length_of_snake).BringToFront()
+    End Sub
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        Select Case e.KeyChar
+            Case "a"
+                left_right_mover = -10
+                up_down_mover = 0
+            Case "d"
+                left_right_mover = 10
+                up_down_mover = 0
+            Case "w"
+                up_down_mover = -10
+                left_right_mover = 0
+            Case "s"
+                up_down_mover = 10
+                left_right_mover = 0
+        End Select
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles tm_snakeMover.Tick
+
+        For i = length_of_snake To 1 Step -1
+            snake(i).Top = snake(i - 1).Top
+            snake(i).Left = snake(i - 1).Left
+        Next
+
+        snake(0).Top += up_down_mover
+        snake(0).Left += left_right_mover
+        collide_with_walls()
+        collide_with_mouse()
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        create_head()
+        create_mouse()
+        tm_snakeMover.Start()
+    End Sub
+
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub pb_field_Click(sender As Object, e As EventArgs) Handles pb_field.Click
+
+    End Sub
+End Class
